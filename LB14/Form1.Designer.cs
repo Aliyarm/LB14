fa01c580cab03b2e78746bd75b9f129cd116ee1a
Алияр Молдабеков <aliyar.m03@gmail.com>
2021-12-20 06:05:52 +0700

    Добавьте файлы проекта.

---
+++ LB14/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿
+namespace LB14
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(38, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.ScrollAlwaysVisible = true;
+            this.listBox1.Size = new System.Drawing.Size(291, 484);
+            this.listBox1.TabIndex = 0;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 20;
+            this.listBox2.Location = new System.Drawing.Point(335, 12);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.ScrollAlwaysVisible = true;
+            this.listBox2.Size = new System.Drawing.Size(290, 484);
+            this.listBox2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(39, 502);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(290, 153);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Сформировать массив из случайных чисел и выполнить поиск элемента.";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(335, 502);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(290, 153);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Отсортировать массив и выполнить поиск элемента.";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(631, 35);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(198, 27);
+            this.textBox1.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(631, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(207, 20);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Введите элемент для поиска";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(866, 665);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ LB14/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LB14
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int[] array = new int[100];
+        Random rand = new();
+        int findValue, count;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            findValue = Convert.ToInt32(textBox1.Text);
+            listBox1.Items.Clear();
+            listBox2.Items.Clear();
+            count = 0;
+            listBox1.Items.Add("Исходный массив:");
+            for (int i = 0; i < 100; i++)
+            {
+                array[i] = rand.Next(-100, 100);
+                listBox1.Items.Add("array[" + i + "] = " + array[i].ToString());
+            }
+            for (int i = 0; i < array.Length; i++)
+            {
+                count++;
+                if (array[i] == findValue)
+                {
+                    MessageBox.Show("Индекс элемета: " + i + "; Количество итераций: " + count + ".");
+                    break;
+                }
+                else if (i == array.Length - 1)
+                {
+                    MessageBox.Show("Нет нужного элемента.");
+                }
+            }
+        }
+
+        private int IndexOf(ref int[] Array, int Value, int Left, int Right)
+        {
+            int x = (Left + Right) / 2;
+            count++;
+            if (Array[x] == Value)
+                return x;
+            if ((x == Left) || (x == Right))
+                return -1;
+            if (Array[x] < Value)
+                return IndexOf(ref Array, Value, x, Right);
+            else
+                return IndexOf(ref Array, Value, Left, x);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox2.Items.Clear();
+            count = 0;
+            listBox2.Items.Add("Отсортированный массив:");
+            for (int i = 0; i < 100; i++)
+            {
+                for (int j = i + 1; j < 100; j++)
+                {
+                    if (array[i] > array[j])
+                    {
+                        int temp = array[i];
+                        array[i] = array[j];
+                        array[j] = temp;
+                    }
+                }
+                listBox2.Items.Add("array[" + i + "] = " + array[i].ToString());
+            }
+            MessageBox.Show("Индекс элемента: " + IndexOf(ref array, findValue, 0, array.Length - 1).ToString());
+        }
+    }
+}
